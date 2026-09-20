77c8678dd1027aa344036685b32ab7e424b7ea0b
Ranajoy66 <indranil.ranajoy66@gmail.com>
2026-09-20 22:45:50 +0530

    modification in base URL

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://dev.starserviceinventory.cloud/api";
+// export const BASE_URL = import.meta.env.VITE_API_URL || "https://dev.starserviceinventory.cloud/api";
+export const BASE_URL = import.meta.env.VITE_API_URL
 
 export const getAuthHeaders = () => ({
   "Content-Type": "application/json",
